1c6358b494a7265af705fbcfcb1460846c7812b5
liuxin <lx_0308@163.com>
2019-08-23 17:32:36 +0800

    use sqlalchemy

--- hello.py
+++ hello.py
@@ -2,82 +2,83 @@ from datetime import datetime
 
 from flask import Flask, request, make_response, redirect, abort, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate, MigrateCommand
 from flask_moment import Moment
-from flask_script import Manager
-from flask_wtf import Form
+from flask_script import Manager, Server, Shell
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:LIUXIN123@127.0.0.1:3306/libra'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['DEBUG'] = True
 
 manager = Manager(app)
-
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 
-def load_user(id):
-    return None
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
 
 
-class NameForm(Form):
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+# @app.shell_context_processor
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
-# @app.route('/')
-# def index():
-#     return '<h1>Hello World!</h1>'
-
-
-# @app.route('/')
-# def index():
-#     user_agent = request.headers.get('User-Agent')
-#     return '<p>Your browser is %s</p>' % user_agent
-
-# @app.route('/')
-# def index():
-#     return '<h1>Bad Request</h1>', 400
-
-# @app.route('/')
-# def index():
-#     response = make_response('<h1>This document carries a cookie!</h1>')
-#     response.set_cookie('answer', '42')
-#     return response
-
-# @app.route('/')
-# def index():
-#     return redirect('https://www.baidu.com')
-
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return '<h1>Hello,%s!</h1>' % name
-
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False),
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
-
-@app.route('/user/<id>')
-def get_user(id):
-    user = load_user(id)
-    if not user:
-        abort(404)
-    return '<h1>Hello,%s</h1>' % user.name
 
 
 @app.errorhandler(404)
@@ -91,5 +92,22 @@ def internal_server_error(e):
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
     manager.run()
+    # db.create_all()
+#
+# admin_role = Role(name='Amin')
+# mod_role = Role(name='Moderator')
+# user_role = Role(name='User')
+#
+# user_john = User(username='john', role=admin_role)
+# user_susan = User(username='susan', role=mod_role)
+# user_david = User(username='david', role=user_role)
+#
+# db.session.add(admin_role)
+# db.session.add(mod_role)
+# db.session.add(user_role)
+# db.session.add(user_john)
+# db.session.add(user_susan)
+# db.session.add(user_david)
+#
+# db.session.commit()
