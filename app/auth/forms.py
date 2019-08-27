aedcc924def1decacf7c137efc8a83aa3580de62
liuxin <lx_0308@163.com>
2019-08-27 16:47:13 +0800

    add user register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,11 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo, ValidationError
+
+from ..models import User
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(),Length(1,64),Email()])
-    password = PasswordField('Password',validators=[DataRequired()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+    email = StringField('邮箱', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('保持登录')
+    submit = SubmitField('登录')
 
+
+class RegistrationForm(FlaskForm):
+    email = StringField('邮箱', validators=[DataRequired(), Length(1, 64), Email()])
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                                                    '用户名仅由字母，数字，_构成')])
+    password = PasswordField('密码', validators=[DataRequired(), EqualTo('password2', message="两次密码不一致")])
+    password2 = PasswordField('确认密码', validators=[DataRequired()])
+    submit = SubmitField('注册')
+
+    def validata_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已存在！')
+
+    def validata_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("用户已存在！")
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,8 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
 
-from .forms import LoginForm
+from .. import db
+from .forms import LoginForm, RegistrationForm
 from . import auth
 from ..models import User
 
@@ -24,3 +25,14 @@ def logout():
     logout_user()
     flash('您已经退出登录')
     return redirect(url_for('main.index'))
+
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        flash("您现在可以登录了！")
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
